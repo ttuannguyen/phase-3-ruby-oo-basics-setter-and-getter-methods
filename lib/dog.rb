5527527a58ad7166ccaf3fdf704cec91e6d5aa31
Tuan Nguyen <trentontuannguyen@gmail.com>
2022-06-15 18:54:39 -0400

    commit tree a3ac470f6dbd52e7694271e7e239f86efe68cd6e

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,19 @@
 class Dog
+    # setter method
+    def name=(name)
+        @name = name 
+    end
 
+    def breed=(breed)
+        @breed = breed
+    end
+
+    # getter method
+    def name
+        @name
+    end
+
+    def breed
+        @breed
+    end
 end
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,20 @@
 class Person
 
+    #setter method
+    def name=(name)
+        @name = name
+    end
+    def job=(job)
+        @job = job
+    end
+
+    #getter method
+    def name 
+        @name
+    end
+
+    def job
+        @job
+    end
+
 end
